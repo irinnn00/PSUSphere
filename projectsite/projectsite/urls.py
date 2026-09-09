fb698c81292c785ac8c17d8aa39007f22757fcc9
irinnn00 <202330198@psu.palawan.edu.ph>
2026-09-10 01:45:31 +0800

    urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -23,7 +23,7 @@ from studentorg.views import HomePageView, OrganizationList, OrganizationCreateV
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('form.html', views.forms_view, name='forms'),
+    path('forms.html', views.forms_view, name='forms'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
