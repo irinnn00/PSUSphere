96f663273869d4b21cbbd39a2f64366ede27c855
irinnn00 <202330198@psu.palawan.edu.ph>
2026-09-10 01:33:21 +0800

    modify urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg import views
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('forms.html', views.forms_view, name='forms'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
