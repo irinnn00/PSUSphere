b135de99409ab5110f83556ba4ddb9057b590755
irinnn00 <202330198@psu.palawan.edu.ph>
2026-09-10 21:16:26 +0800

    modify views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -41,6 +41,8 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    
+    
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -150,6 +152,13 @@ class ProgramListView(ListView):
     context_object_name = 'programs'
     template_name = 'program_list.html'
     paginate_by = 10
+
+    def get_ordering(self):
+        allowed = ["prog_name", "college__college_name"]
+        sort_by = self.request.GET.get("sort_by")
+        if sort_by in allowed:
+            return sort_by
+        return "prog_name"
 
 class ProgramCreateView(CreateView):
     model = Program
